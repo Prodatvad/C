f1e29911bcb51d5b2f6d75d13c8cf67395d3b326
Prodatvad <vadim53958821@yandex.ru>
2023-03-16 21:19:22 +0300

    New change in 5 branch

--- main.c
+++ main.c
@@ -1,79 +1,28 @@
 //tetris
 
 #include <stdio.h>
-#include <unistd.h>
 
-int X;
-int Y;
-void movement(char key);
-void printmap();
-//void square();
-//void stick();
-
+ 
 int main() {
-
-printmap();
-
-while(1){
-char c = _getch();                               //getch with _
-//sleep(3);
-movement(c);
-printmap();
-}
-
-
-return 0;
-
-}
-
-
-
-
-
-
-
-int X = 1; int Y = 1; //1
-
-void square() { //2
-puts("X\nX");
-}
-
-void stick() { //3
-puts("XX");
-}
-
-void printmap() { //4 map for player
-for(int y = 1; y<=4; y++) {
-
-for(int x = 1; x<=4; x++) {
 char point = '.';
-
-if(x == X && y == Y) {
-char player = 'X';
-point = player;
-printf("%c ", point); }
-else{printf("%c ", point);}}
-printf("\n");}
+int i, j;
+ 
+char mass[10][12] = {};
+ 
+//printf("%c\n", mass[10][10]);
+ 
+for(j = 0; j < 10; j++) {
+ 
+for(i = 0; i < 12; i++) {
+    
+    }
+ 
 }
-
-void movement(char key) { //5 switch case
-switch(key) {
-case 'w':
-Y--;
-break;
-
-case 's':
-Y++;
-break;
-
-case 'a':
-X--;
-break;
-
-case 'd':
-X++;
-break;
-}
-
-printf("\n");
-}
+ 
+printf("[%d][%d] = [%c]  \n\n\n", j, i, mass[j][i]);
+ 
+ 
+ 
+ 
+ 
+return 0;}
