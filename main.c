242b0031cbfd9229e31d1548166669dfa7e94b88
Pupsik <112538615+Prodatvad@users.noreply.github.com>
2023-05-11 02:01:06 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,45 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//Matrix
+int main(void) {
+    int i, j, l, k;
+    int mas1[3][3];
+    int mas2[3][3];
+    int mas3[3][3];
+    int a;
 
-int main() {
-
-int i, j, l, k;
-int mas1[3][3];
-int mas2[3][3];
-int mas3[3][3];
-int a;
-
-printf("\nInput1: ");
-for(i = 0; i < 3; i++){ // Функция заполнения mas1
-    for(j = 0; j < 3; j++) {
-        scanf("%d", &a);
-        mas1[i][j] = a;
+    printf("\nInput1: ");
+    for(i = 0; i < 3; i++){ // Функция заполнения mas1
+        for(j = 0; j < 3; j++) {
+            scanf("%d", &a);
+            mas1[i][j] = a;
+        }
     }
-}
 
-printf("\nInput2: ");
-for(i = 0; i < 3; i++){ // Функция заполнения mas2
-    for(j = 0; j < 3; j++) {
-        scanf("%d", &a);
-        mas2[i][j] = a;
+    printf("\nInput2: ");
+    for(i = 0; i < 3; i++){ // Функция заполнения mas2
+        for(j = 0; j < 3; j++) {
+            scanf("%d", &a);
+            mas2[i][j] = a;
+        }
     }
-}
 
-for(i = 0; i < 3; i++){ // Функция вычисления mas3
-    for(j = 0; j < 3; j++) {
-        mas3[i][j] = mas1[i][j] - mas2[i][j];
+    for(i = 0; i < 3; i++){ // Функция вычисления mas3
+        for(j = 0; j < 3; j++) {
+            mas3[i][j] = mas1[i][j] - mas2[i][j];
+        }
     }
-}
-printf("\nResult: ");
+    printf("\nResult: ");
 
-for(i = 0; i < 3; i++){ // Функция выведения mas3
-    for(j = 0; j < 3; j++) {
-        printf("%d ", mas3[i][j]);
+    for(i = 0; i < 3; i++){ // Функция выведения mas3
+        for(j = 0; j < 3; j++) {
+            printf("%d ", mas3[i][j]);
+        }
     }
-}
-printf("\n");
+    printf("\n");
 
-return 0;
+    return 0;
 } 
