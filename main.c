916da2e7f72a612eeef1bdc0cc20c0b93c1365b0
Pupsik <112538615+Prodatvad@users.noreply.github.com>
2023-05-11 01:42:20 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,5 +1,3 @@
-//1
-
 #include <stdio.h>
 
 int main(void) {
@@ -9,16 +7,13 @@ int main(void) {
     int mult;
     int n3 = num % 10;
     int n1 = num / 100;
-    int n2 = (num / 10)%10;
+    int n2 = (num / 10) % 10;
     mult = n1 * n2 * n3;
 
-    if(num%3==0){
-        printf("%d => да", mult);
+    if(num % 3 == 0){
+        printf("%d => yes", mult);
     }
-
-    else{
-        printf("%d => нет", mult);
-    }
-
+    printf("%d => no", mult);
+    
     return 0;
 }
