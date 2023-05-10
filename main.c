89c9c00e3d133b718b6cada0160b0322d4aee7fc
Pupsik <112538615+Prodatvad@users.noreply.github.com>
2023-05-11 01:58:41 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,57 +1,59 @@
-//4 game with player X
-
 #include<stdio.h>
 
 int X;
 int Y;
 void printmap();
 
-int main() {
-    
-printmap();
+int main() {    
+    printmap();
 
-char movement;
-char w = 'w';
-char s = 's';
-char a = 'a';
-char d = 'd';
+    char movement;
+    char w = 'w';
+    char s = 's';
+    char a = 'a';
+    char d = 'd';
 
-while(1){
-    scanf("%c", &movement);
-switch(movement) {
-    case 'w':
-    Y--;
-    break;
+    while(1) {
+        scanf("%c", &movement);
+        switch(movement) {
+            case 'w':
+            Y--;
+            break;
+
+            case 's':
+            Y++;
+            break;
+
+            case 'a':
+            X--;
+            break;
+
+            case 'd':
+            X++;
+            break;
+        }    
+        printmap();
+        printf("\n");
+    }
     
-    case 's':
-    Y++;
-    break;
-    
-    case 'a':
-    X--;
-    break;
-    
-    case 'd':
-    X++;
-    break;}
-    
-printmap();
-printf("\n");}
-return 0;
+    return 0;
 }
-
 int X = 3;
 int Y = 3;
 
 void printmap() {
-for(int y = 0; y<=6; y++) {
- 
-  for(int x = 0; x<=6; x++) {
-  char point = '.';
-
-    if(x == X && y == Y) {
-    char player = 'X';
-    point = player;
-    printf("%c ", point); }
-        else{printf("%c ", point);}}
-printf("\n");}}
+    for(int y = 0; y<=6; y++) { 
+        for(int x = 0; x<=6; x++) {
+            char point = '.';
+            if(x == X && y == Y) {
+                char player = 'X';
+                point = player;
+                printf("%c ", point); 
+            }
+            else {
+                printf("%c ", point);
+            }
+        }
+printf("\n");
+    }
+}
