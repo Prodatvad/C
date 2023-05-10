b157fbfff845ad900996759e40cca3f13a1f6a6c
Pupsik <112538615+Prodatvad@users.noreply.github.com>
2023-05-11 02:07:31 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,47 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//Matrix 3
-
 void inputmas();
 
-int main() {
+int main(void) {
+    inputmas();
 
-inputmas();
-
-return 0;
+    return 0;
 }
 
 void inputmas() {
+    int size_1, size_2;
 
-int size1, size2;
+    printf("Size 1: ");
+    scanf("%d", &size_1);
 
-printf("Size 1: ");
-scanf("%d", &size1);
+    printf("Size 2: ");
+    scanf("%d", &size_2);
 
-printf("Size 2: ");
-scanf("%d", &size2);
+    int mas1[size_1][size_2];
+    int mas2[size_1][size_2];
 
-int mas1[size1][size2];
-int mas2[size1][size2];
-
-printf("Input mas: ");
-    for(int i = 0; i < size1; i++) { //Функция ввода
-        for(int j = 0; j < size2; j++) {
-        int a;
-        scanf("%d", &a);
-        mas1[i][j] = a;
+    printf("Input mas: ");
+    for(int i = 0; i < size_1; i++) {                //Функция ввода
+        for(int j = 0; j < size_2; j++) {
+            int a;
+            scanf("%d", &a);
+            mas1[i][j] = a;
         }
     }
 
-printf("Output mas: \n");
-for(int i = 0; i < size1; i++) { //Функция транспонирования с выводом
-    for(int j = 0; j < size2; j++) {
-        mas2[i][j] = mas1[j][i];
-        printf("%d ", mas2[i][j]);
+    printf("Output mas: \n");
+    for(int i = 0; i < size_1; i++) {                //Функция транспонирования с выводом
+        for(int j = 0; j < size_2; j++) {
+            mas2[i][j] = mas1[j][i];
+            printf("%d ", mas2[i][j]);
+        }
+        printf("\n");
     }
-    printf("\n");
-}
-
-return 0;
+    return 0;
 }
