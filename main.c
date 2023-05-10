4d9864ee534be6901a96dcd769c8ef46406aa03a
Pupsik <112538615+Prodatvad@users.noreply.github.com>
2023-05-11 02:02:11 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,36 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//Matrix 2
+int main(void) {
+    int i, j;
+    int mas1[3][3];
+    int mas2[3][3];
+    int a, b;
 
-int main() {
-int i, j;
-int mas1[3][3];
-int mas2[3][3];
-int a, b;
-
-printf("Input1: ");
-for(i = 0; i < 3; i++) { // Функция занесения делимых чисел в массив 1
-    for(j = 0; j < 3; j++) {
-        scanf("%d", &a);
-        mas1[i][j] = a;
+    printf("Input1: ");
+    for(i = 0; i < 3; i++) {                // Функция занесения делимых чисел в массив 1
+        for(j = 0; j < 3; j++) {
+            scanf("%d", &a);
+            mas1[i][j] = a;
+        }
     }
-}
 
-printf("Input2: ");
-scanf("%d", &b); //Ввод делителя
-for(i = 0; i < 3; i++) { //Функция вычисления частных и занесение их в массив 2
-    for(j = 0; j < 3; j++) {
-        mas2[i][j] = mas1[i][j] / b;
+    printf("Input2: ");
+    scanf("%d", &b);                        //Ввод делителя
+    for(i = 0; i < 3; i++) {                //Функция вычисления частных и занесение их в массив 2
+        for(j = 0; j < 3; j++) {
+            mas2[i][j] = mas1[i][j] / b;
+        }
     }
-}
 
-printf("Result: ");
-for(i = 0; i < 3; i++) { //Функция выведения частных из массива 2
-    for(j = 0; j < 3; j++) {
-        printf("%d ", mas2[i][j]);
+    printf("Result: ");
+    for(i = 0; i < 3; i++) {                //Функция выведения частных из массива 2
+        for(j = 0; j < 3; j++) {
+            printf("%d ", mas2[i][j]);
+        }
     }
-}
 
-return 0;
+    return 0;
 }
